115d6ec434b4a78b7d12df817fb581c66138f996
FIELDGUITAR2 <josagoor@gmail.com>
2025-11-13 14:50:40 -0500

    Cambios Grandes

--- actualizarEstadoCitaAjax.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require ("logica/Cita.php");
-$idCita = $_GET["idCita"];
-$idNuevoEstadoCita = $_GET["idNuevoEstadoCita"];
-$valorNuevoEstadoCita = $_GET["valorNuevoEstadoCita"];
-$cita = new Cita($idCita);
-$cita -> actualizarEstado($idNuevoEstadoCita);
-echo $valorNuevoEstadoCita;
-?>
